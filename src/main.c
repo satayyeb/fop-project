a1785d5287ea1f692ff82c740f6e5e4698f662dd
satayyeb <salitayyeb@gmail.com>
2022-02-03 17:20:22 +0330

    add scan keyboard for main menu

--- src/main.c
+++ src/main.c
@@ -13,7 +13,10 @@
 #define max_land_size  80
 #define min_land_size  50
 #define ATTACK_LIMIT    10
-#define DARK_PURPLE 0xaa702666
+#define DARK_GREEN 0xaa702666
+#define LIGHT_GREEN 0xffde3ec9
+#define DARK_RED 0xaa09099e
+#define LIGHT_RED 0xff3535e8
 
 const int FPS = 60;
 const int section = 50;
@@ -57,13 +60,15 @@ int random_between(int start, int end) {
 
 void draw_the_map(APP *app, POINT *array, int number_of_points) {
     char str[6];
+
+
     for (int i = 0; i < number_of_points; i++) {
         if (array[i].ownership == 1) {
-            filledCircleColor(app->renderer, array[i].x, array[i].y, array[i].r, 0xffde3ec9);
-            filledCircleColor(app->renderer, array[i].x, array[i].y, 20, 0xaa702666);
+            filledCircleColor(app->renderer, array[i].x, array[i].y, array[i].r, LIGHT_GREEN);
+            filledCircleColor(app->renderer, array[i].x, array[i].y, 20, DARK_GREEN);
         } else if (array[i].ownership == 2) {
-            filledCircleColor(app->renderer, array[i].x, array[i].y, array[i].r, 0xff3535e8);
-            filledCircleColor(app->renderer, array[i].x, array[i].y, 20, 0xaa09099e);
+            filledCircleColor(app->renderer, array[i].x, array[i].y, array[i].r, LIGHT_RED);
+            filledCircleColor(app->renderer, array[i].x, array[i].y, 20, DARK_RED);
         } else {
             filledCircleColor(app->renderer, array[i].x, array[i].y, array[i].r, 0xffb7edba);
             filledCircleColor(app->renderer, array[i].x, array[i].y, 20, 0xaa2e8233);
@@ -197,24 +202,35 @@ void move(APP *app, POINT *array, SOLDIER **soldiers) {
             if (soldiers[i][j].launch_counter < 10) {
                 soldiers[i][j].launch_counter += 1;
                 break;
-            } else if (soldiers[i][j].launch_counter == 10) {
-                //decrease one soldier
-                array[soldiers[i][j].start_point].value -= 1;
-                soldiers[i][j].launch_counter += 1;
             }
-
-
 
             //move the soldier and save its position
             move_the_soldier(app, array, soldiers, i, j);
+
+            //the soldiers will be killed if they reached to each other
+            for (int k = 0; k < ATTACK_LIMIT; k++) {
+                if (soldiers[k] == NULL)
+                    continue;
+                for (int l = 0; soldiers[k][l].killed != -1; l++) {
+                    if (soldiers[k][l].ownership != soldiers[i][j].ownership &&
+                        abs(soldiers[k][l].x - soldiers[i][j].x) < 10 &&
+                        abs(soldiers[k][l].y - soldiers[i][j].y) < 10) {
+                        soldiers[i][j].killed = 1;
+                        soldiers[k][l].killed = 1;
+                        soldiers[k][l].x = 0;
+                        soldiers[i][j].x = 0;
+                    }
+                }
+            }
 
             //if the soldier reached the destination
             int dest_x = array[soldiers[i][j].end_point].x;
             int dest_y = array[soldiers[i][j].end_point].y;
             if (abs(soldiers[i][j].x - dest_x) < 8 && abs(soldiers[i][j].y - dest_y) < 8) {
-                //TODO increase the number of soldiers in state
-                if (array[soldiers[i][j].end_point].value == 0 || array[soldiers[i][j].end_point].ownership == 1) {
-                    array[soldiers[i][j].end_point].ownership = 1;
+                if (array[soldiers[i][j].end_point].value == 0) {
+                    array[soldiers[i][j].end_point].ownership = soldiers[i][j].ownership;
+                    array[soldiers[i][j].end_point].value += 1;
+                } else if (array[soldiers[i][j].end_point].ownership == soldiers[i][j].ownership) {
                     array[soldiers[i][j].end_point].value += 1;
                 } else {
                     array[soldiers[i][j].end_point].value -= 1;
@@ -227,7 +243,13 @@ void move(APP *app, POINT *array, SOLDIER **soldiers) {
                     return;
                 }
             }
-            filledCircleColor(app->renderer, soldiers[i][j].x, soldiers[i][j].y, 5, DARK_PURPLE);
+            //print the soldiers
+            if (soldiers[i][j].killed != 1) {
+                if (soldiers[i][j].ownership == 1)
+                    filledCircleColor(app->renderer, soldiers[i][j].x, soldiers[i][j].y, 5, DARK_GREEN);
+                else
+                    filledCircleColor(app->renderer, soldiers[i][j].x, soldiers[i][j].y, 5, DARK_RED);
+            }
         }
     }
 }
@@ -235,6 +257,9 @@ void move(APP *app, POINT *array, SOLDIER **soldiers) {
 
 void make_soldier(POINT *array, int number_of_points) {
     for (int i = 0; i < number_of_points; i++) {
+//        if (array[i].ownership == 0 && array[i].value < 10) {
+//            array[i].value += 1;
+//        }
         if (array[i].ownership == 1 && array[i].value < 50) {
             array[i].value += 1;
         }
@@ -244,25 +269,79 @@ void make_soldier(POINT *array, int number_of_points) {
     }
 }
 
+void present_main_menu(APP *app) {
+    SDL_Event event;
+    char ch[2] = {'\0'};
+    bool shift = false;
+    bool lock = false;
+    bool quit = false;
+    char text[100] = {'\0'};
+    while (!quit) {
+
+        SDL_PollEvent(&event);
+
+        switch (event.type) {
+            case SDL_QUIT:
+                quit = true;
+                die(app);
+                break;
+
+            case SDL_KEYUP:
+                if (event.key.keysym.sym == SDLK_RSHIFT || event.key.keysym.sym == SDLK_LSHIFT) {
+                    shift = false;
+                }
+                lock = false;
+                break;
+
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_RETURN) {
+                    quit = true;
+                    break;
+                } else if (event.key.keysym.sym == SDLK_RSHIFT || event.key.keysym.sym == SDLK_LSHIFT) {
+                    shift = true;
+                } else if (!lock && event.key.keysym.sym == SDLK_BACKSPACE && strlen(text) > 0) {
+                    text[strlen(text) - 1] = '\0';
+                    lock = true;
+                } else if (event.key.repeat == 0 && (32 <= event.key.keysym.sym) && (event.key.keysym.sym <= 127)) {
+                    ch[0] = (char) event.key.keysym.sym;
+                    if (shift && 'a' <= ch[0] && ch[0] <= 'z') {
+                        ch[0] = (char) ((int) ch[0] - 32);
+                    }
+                    strcat(text, ch);
+                    lock = true;
+                }
+                break;
+        }
+        SDL_Delay(40);
+        SDL_SetRenderDrawColor(app->renderer, 252, 250, 217, 255);
+        SDL_RenderClear(app->renderer);
+        stringRGBA(app->renderer, 200, 200, text, 0, 4, 4, 255);
+        SDL_RenderPresent(app->renderer);
+    }
+}
+
 
 int main() {
     APP *app = calloc(1, sizeof(APP));
     SDL_Event event;
     bool quit = false;
     srand(time(0));
-    POINT array[15];
+    POINT array[15] = {0};
 
     if (!init(app)) {
         die(app);
     }
+
+    present_main_menu(app);
 
     SDL_SetRenderDrawColor(app->renderer, 252, 255, 217, 255);
     SDL_RenderClear(app->renderer);
     int number_of_points = generate_random_map(array, app);
     int starting_point = -1;
     SOLDIER *soldiers[10] = {NULL};
-
     int counter = 0;
+
+    quit = false;
     while (!quit) {
         SDL_PollEvent(&event);
 
@@ -274,7 +353,7 @@ int main() {
             case SDL_MOUSEBUTTONDOWN:
                 if (event.button.button == SDL_BUTTON_LEFT && starting_point == -1) {
                     int point_number = witch_point(event.button.x, event.button.y, array, number_of_points);
-                    if (array[point_number].ownership == 1) {
+                    if (array[point_number].ownership == 1 || array[point_number].ownership == 2) {
                         printf("button down\n");
                         starting_point = point_number;
                     }
@@ -287,11 +366,13 @@ int main() {
                     if (ending_point != -1 && ending_point != starting_point) {
                         printf("button up\n");
                         int number_of_soldiers = array[starting_point].value;
+                        array[starting_point].value = 0;
                         int i = 0;
                         while (soldiers[i] != NULL)
                             i++;
                         soldiers[i] = calloc(number_of_soldiers + 1, sizeof(SOLDIER));
                         for (int j = 0; j < number_of_soldiers; j++) {
+                            soldiers[i][j].ownership = array[starting_point].ownership;
                             soldiers[i][j].start_point = starting_point;
                             soldiers[i][j].end_point = ending_point;
                             soldiers[i][j].x = array[starting_point].x;
@@ -310,7 +391,7 @@ int main() {
         counter++;
         if (counter > 50) {
             counter = 0;
-            make_soldier(array,number_of_points);
+            make_soldier(array, number_of_points);
         }
         SDL_SetRenderDrawColor(app->renderer, 252, 255, 217, 255);
         SDL_RenderClear(app->renderer);
@@ -321,3 +402,4 @@ int main() {
     }
     return 0;
 }
+
